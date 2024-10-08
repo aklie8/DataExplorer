8006384bb62c07e0dd708273af72a2bc2817b480
aklile tesfaye <akliletesfaye@akliles-air.ds.usfca.edu>
2024-10-07 17:45:18 -0700

    completed 1st part of project 2

---
+++ lists.py
@@ -0,0 +1,20 @@
+
+# Create a list of movie titles, ratings, and genres:
+movies = ['Inception', 'The Dark Knight', 'Interstellar']
+ratings = [9, 8.5, 9.5]
+genres = ['Sci-Fi', 'Action', 'Drama']
+
+
+
+# Access and modify items in the list:
+# Print the first movie in the list: 
+print(movies[0])
+# Change the rating of 'Inception' to 9.2: 
+ratings[0] = 9.2
+
+
+# Add new movies to the list:
+# Use the append() function to add a new movie to the list:
+movies.append('Tenet')
+ratings.append(7.5)
+genres.append('Thriller')
